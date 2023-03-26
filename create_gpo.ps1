5b98b87978d018263c78ae505328a4390fb0fe1e
ryukk33 <104195483+ryukk33@users.noreply.github.com>
2023-03-26 18:01:45 +0200

    Add files via upload

---
+++ create_gpo.ps1
@@ -0,0 +1,20 @@
+# Create the first GPO to deny users from installing or removing printers
+$gpo1 = New-GPO -Name "Printer Restrictions"
+New-GPPermissions -Guid $gpo1.Id -TargetName "Authenticated Users" -TargetType Group -PermissionLevel GpoApply -Inherited Yes
+Set-GPRegistryValue -Guid $gpo1.Id -Key "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -ValueName "NoAddPrinter" -Type DWORD -Value 1
+Set-GPRegistryValue -Guid $gpo1.Id -Key "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -ValueName "NoDeletePrinter" -Type DWORD -Value 1
+
+# Create the second GPO to deny users from changing the wallpaper
+$gpo2 = New-GPO -Name "Wallpaper Restrictions"
+New-GPPermissions -Guid $gpo2.Id -TargetName "Authenticated Users" -TargetType Group -PermissionLevel GpoApply -Inherited Yes
+Set-GPRegistryValue -Guid $gpo2.Id -Key "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System" -ValueName "Wallpaper" -Type String -Value "C:\Windows\Web\Wallpaper\Windows\img0.jpg"
+Set-GPRegistryValue -Guid $gpo2.Id -Key "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Policies\System" -ValueName "WallpaperStyle" -Type String -Value "0"
+
+# Link the GPOs to the "Auditeurs 1" and "Auditeurs 2" OUs
+$ou1 = Get-ADOrganizationalUnit -Identity "OU=Auditeurs 1,DC=contoso,DC=com"
+$ou2 = Get-ADOrganizationalUnit -Identity "OU=Auditeurs 2,DC=contoso,DC=com"
+New-GPLink -Name "Printer Restrictions" -Target $ou1
+New-GPLink -Name "Printer Restrictions" -Target $ou2
+New-GPLink -Name "Wallpaper Restrictions" -Target $ou1
+New-GPLink -Name "Wallpaper Restrictions" -Target $ou2
+
